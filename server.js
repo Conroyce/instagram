c457df4a6d00496c913b3d45f0962237138bdc7b
Connor Kojimoto <cokojimoto@gmail.com>
2015-01-24 13:17:47 -0800

    added mongo and mongoose

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require("express");
 var app = express();
+var user = require("./routes/user");
 
 app.use(express.static(__dirname+"/assets"));
 app.engine('jade',require('jade').__express);
 app.set("views","./views");
 app.set("view engine", "jade");
 
-app.use("/",function(req,res,next) {
-  res.send("hey");
-});
+app.use("/",user);
+
 
 var port = "8080";
 var server = app.listen(port);
