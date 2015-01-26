0f45eb56b2381647cfce69190580f4e7ca051c35
Connor Kojimoto <cokojimoto@gmail.com>
2015-01-25 20:42:07 -0800

    users can now create photos

--- routes/user.js
+++ routes/user.js
@@ -20,7 +20,7 @@ router.route("/")
     };
     usersFile.push(user);
     fs.writeFileSync("./users.json",JSON.stringify(usersFile));
-    res.render("photos");
+    res.render("photos", {photos: user.photos});
   });
 
 router.route("/:id/photos")
@@ -32,8 +32,24 @@ router.route("/:id/photos")
         photos.push(photo);
       }
     })
-    res.render("photos",{photos:photos});
-  })  
+    console.log(id);
+    res.render("photos",{photos:photos,id:id});
+  })
+  .post(function(req,res,next) {
+    var info = req.body;
+    var photo = {
+      title: info.title,
+      src: info.photo,
+      likes: 0,
+      id: photosFile.length+1,
+      user_id: req.params.id
+    };
+    photosFile.push(photo);
+    fs.writeFileSync("./photos.json",JSON.stringify(photosFile));
+    console.log(photosFile);
+
+    res.render("index");
+  });  
   
 
 module.exports = router  
